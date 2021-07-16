bf148c8e4724c943c5970893e518b7112bc14e8e
yenilikci <m.melihcelik017@outlook.com>
2021-07-16 15:07:35 +0300

    c.Param, c.QueryParam, c.JSON

--- main.go
+++ main.go
@@ -11,11 +11,37 @@ func mainHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Main endpointe get isteği yapıldı.")
 }
 
+//http://localhost:8088/user/data?username=yenilikci&name=melih&surname=celik
+func userHandler(c echo.Context) error {
+	//param
+	dataType := c.Param("data")
+	//query
+	username := c.QueryParam("username")
+	name := c.QueryParam("name")
+	surname := c.QueryParam("surname")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Username: %s, Name: %s, Surname: %s", username, name, surname))
+	}
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"username": username,
+			"name":     name,
+			"surname":  surname,
+		})
+	}
+
+	return c.String(http.StatusBadRequest, "Yalnızca 'json' veya 'string' parametreleri kullanılabilir")
+}
+
 func main() {
 	fmt.Printf("Hello World")
 
 	e := echo.New()
+
 	e.GET("/main", mainHandler)
+
+	e.GET("/user/:data", userHandler)
 
 	e.Start(":8088")
 }
